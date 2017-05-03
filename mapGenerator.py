b97fe425ab5e6d335564d303548d894c1a121ab9
trysomethingelse <ma2tig@gmail.com>
2017-05-03 20:53:41 +0200

    poprawione zapisywanie do XML

--- mapGenerator.py
+++ mapGenerator.py
@@ -4,9 +4,8 @@ import math
 from PyQt5.QtWidgets import QGraphicsItem
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import *
-from PyQt5 import QtGui, QtCore, QtSvg,Qt
+from PyQt5 import QtGui, QtCore, QtSvg, Qt
 from xml.dom import minidom
-
 
 
 class MapGenerator:
@@ -17,7 +16,6 @@ class MapGenerator:
 
     MY_TANK = 1
     NOT_MY_TANK = 0
-
 
     ENEMIES = 1
     ENEMY = -10
@@ -39,7 +37,7 @@ class MapGenerator:
     root = doc.createElement("mapHistory")
     historyStep = 0
 
-    #wymienione typy płytek które nie są ruchome
+    # wymienione typy płytek które nie są ruchome
     TILES_TYPES = 4
     CLEAR_TILE = 0
     DESTRUCTABLE_TILE = 1
@@ -47,16 +45,12 @@ class MapGenerator:
     BULLET_TILE = 3
     otherTiles = []
 
-    #odpowiednia rotacja czołgu to odpowiedni indeks w tabeli
+    # odpowiednia rotacja czołgu to odpowiedni indeks w tabeli
     enemyTiles = []
     myTankTiles = []
 
-
-
-
     def __init__(self):
-        print("init")
-
+        print("map init")
 
     def generate(self):
         self.plane[4, :7] = np.ones([1, 7]) * self.DESTR  # pas zniszczalnych plytek
@@ -65,50 +59,15 @@ class MapGenerator:
         self.plane[0, 0] = self.AGENT
         self.plane[7, 15] = self.ENEMY
         self.plane[3, 23] = self.ENEMY
-    def saveHistory(self):
-        print("zapisywanie_",self.historyStep)
-        moment = self.doc.createElement("moment")
-        moment.setAttribute("step",str(self.historyStep))
 
 
-        for index,element in np.ndenumerate(self.plane):
-            name = "hex_"+str(index[0])+","+str(index[1])
-            change = self.doc.createElement(name)
-            change.setAttribute("map",str(element))#zapis calej mapy
-            moment.appendChild(change)
-        self.root.appendChild(moment)
-        self.doc.appendChild(self.root)
-        self.historyStep += 1
-    def saveDataToXML(self):
-        self.doc.writexml(open('data.xml', 'w'),
-                          indent="  ",
-                          addindent="  ",
-                          newl='\n')
 
-        self.doc.unlink()
     def playHistory(self):
         doc = minidom.parse("data.xml")
+
     def toConsole(self):
         print(self.plane)
-    # def graphicMap(self, handleScene):
-    #     self.svgHEX = [[QtSvg.QSvgWidget() for i in range(0, self.HEIGHT)] for j in range(0, self.WIDTH)]
-    #
-    #     for index, element in np.ndenumerate(self.svgHEX):
-    #         self.svgHEX[index[0]][index[1]] = self.memoryEmptyTile
-    #
-    #         offsetX = 3 / 2 * self.TILE_WIDTH * index[0]
-    #
-    #         if index[1] % 2 == 1:  # przesuniecie o staly offset
-    #             offsetX += 3 / 4 * self.TILE_WIDTH
-    #
-    #         offsetY = index[1] * self.TILE_HEIGHT / 2
-    #
-    #         self.svgHEX[index[0]][index[1]].setGeometry(self.startX + offsetX,
-    #                                                     self.startY + offsetY,
-    #                                                     self.TILE_WIDTH,
-    #                                                     self.TILE_HEIGHT)  # odpowiada za rysowanie płytki w odpowiednim miejscu
-    #         self.svgHEX[index[0]][index[1]].setStyleSheet("background-color:transparent;")
-    #         handleScene.addWidget(self.svgHEX[index[0]][index[1]])
+
     def graphicMap(self, handleScene):
         self.tilesToMemory()
 
@@ -118,7 +77,7 @@ class MapGenerator:
 
             self.pngHEX[index[0]][index[1]].setPixmap(self.otherTiles[self.CLEAR_TILE])
 
-            #obliczenia pozycji
+            # obliczenia pozycji
             offsetX = 3 / 2 * self.TILE_WIDTH * index[0]
 
             if index[1] % 2 == 1:  # przesuniecie o staly offset
@@ -139,24 +98,22 @@ class MapGenerator:
         self.otherTiles.append(QtGui.QPixmap("./images/hexBrickDestr.png"))
         self.otherTiles.append(QtGui.QPixmap("./images/hexBrickNonDestr.png"))
         self.otherTiles.append(QtGui.QPixmap("./images/hexBullet.png"))
-        for index in range(self.TILES_TYPES):#ustawia odpowiednie rozmiary płytek
+        for index in range(self.TILES_TYPES):  # ustawia odpowiednie rozmiary płytek
             self.otherTiles[index] = self.otherTiles[index].scaled(self.TILE_WIDTH, self.TILE_HEIGHT)
 
-        #płytki czołgów
-        for index in range(6):#wszystkie możliwe ustawienia kąta
+        # płytki czołgów
+        for index in range(6):  # wszystkie możliwe ustawienia kąta
             self.myTankTiles.append(QtGui.QPixmap('./images/myTank/hexMyTank' + str(index) + '.png'))
             self.myTankTiles[index] = self.myTankTiles[index].scaled(self.TILE_WIDTH,
-                                                                     self.TILE_HEIGHT)#przestaw rozmiar
+                                                                     self.TILE_HEIGHT)  # przestaw rozmiar
 
             self.enemyTiles.append(QtGui.QPixmap('./images/enemy/hexEnemy' + str(index) + '.png'))
             self.enemyTiles[index] = self.enemyTiles[index].scaled(self.TILE_WIDTH,
                                                                    self.TILE_HEIGHT)  # przestaw rozmiar
 
-
-
     def planeToGraphics(self, tank):  # odświeża całą mapę
         for index, element in np.ndenumerate(self.plane):
-            tile = self.otherTiles[self.CLEAR_TILE]#jesli płytka pusta
+            tile = self.otherTiles[self.CLEAR_TILE]  # jesli płytka pusta
 
             if element == self.AGENT:
                 tile = self.myTankTiles[tank.rotation]
@@ -170,24 +127,46 @@ class MapGenerator:
                 tile = self.otherTiles[self.BULLET_TILE]  # jesli płytka pusta
 
             self.pngHEX[index[0]][index[1]].setPixmap(tile)
+        self.mapChange()  # wywoływana w każdej zmianie mapy
 
     def tankRefresh(self, tank, isMy):
 
-        if isMy==1:
+        if isMy == 1:
             tile = self.myTankTiles[tank.rotation]
         else:
             tile = self.enemyTiles[tank.rotation]
 
         self.pngHEX[tank.oldPos[0]][tank.oldPos[1]].setPixmap(self.otherTiles[self.CLEAR_TILE])
         self.pngHEX[tank.position[0]][tank.position[1]].setPixmap(tile)
-        self.saveHistory()
+        self.mapChange()  # wywoływana w każdej zmianie mapy
 
-
-
-    def tileRefresh(self, tile, newType): #używane do podmieniania płytki
+    def tileRefresh(self, tile, newType):  # używane do podmieniania płytki
         if (newType == self.EMPTY):
-           self.pngHEX[tile[0]][tile[1]].setPixmap(self.otherTiles[self.CLEAR_TILE])
+            self.pngHEX[tile[0]][tile[1]].setPixmap(self.otherTiles[self.CLEAR_TILE])
         if (newType == self.BULLET):
-           self.pngHEX[tile[0]][tile[1]].setPixmap(self.otherTiles[self.BULLET_TILE])
+            self.pngHEX[tile[0]][tile[1]].setPixmap(self.otherTiles[self.BULLET_TILE])
+        self.mapChange()  # wywoływana w każdej zmianie mapy
 
         self.saveHistory()
+
+    def mapChange(self):# wywoływana w każdej zmianie mapy
+        self.saveHistory()
+
+    def saveHistory(self):
+        print("zapisywanie_", self.historyStep)
+        moment = self.doc.createElement("moment")
+        moment.setAttribute("step", str(self.historyStep))
+
+        for index, element in np.ndenumerate(self.plane):
+            name = "hex_" + str(index[0]) + "," + str(index[1])
+            change = self.doc.createElement(name)
+            change.setAttribute("map", str(element))  # zapis calej mapy
+            moment.appendChild(change)
+        self.root.appendChild(moment)
+        self.doc.appendChild(self.root)
+        self.historyStep += 1
+    def saveDataToXML(self):
+        self.doc.writexml(open('data.xml', 'w'),
+                          indent="  ",
+                          addindent="  ",
+                          newl='\n')
